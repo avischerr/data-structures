4069c2648d73525d504cf2c04171a5dc23e84604
Arielle <ariellescherr@gmail.com>
2020-08-20 16:35:35 -0500

    Functional Shared - Queue complete / Functional - Queue & Stack complete

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,55 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var instance = {};
+  instance.storage = {};
+  _.extend(instance, queueMethods);
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  // use "this"
 
+  // enqueue: add a string to the back of the queue
+  enqueue: function (value) {
+    var keys = Object.keys(this.storage);
+    var newKey;
 
+    if (keys.length === 0) {
+      newKey = 0;
+    } else {
+      newKey = keys.length;
+    }
+
+    this.storage[newKey] = value;
+    return this.storage;
+  },
+
+  // Remove and return the string at the front of the queue
+  dequeue: function () {
+    var result;
+    var keys = Object.keys(this.storage);
+
+    if (keys.length > 0) {
+      result = this.storage[0];
+      delete this.storage[0];
+
+      for (var index in this.storage) {
+        var newKey = parseInt(index) - 1;
+        newKey = JSON.stringify(newKey);
+        this.storage[newKey] = this.storage[index];
+        delete this.storage[index];
+      }
+
+    } else {
+      result = null;
+    }
+
+    return result;
+  },
+
+  size: function () {
+    return Object.keys(this.storage).length;
+  }
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,14 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // var someInstance = {};
+  // var storage = {};
+
+  // push: stackMethods.push(),
+  // return obj.method = s
+
+  // return someInstance;
 };
 
 var stackMethods = {};
