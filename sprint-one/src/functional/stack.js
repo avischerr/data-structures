7f04f8deca17b78118f66c9ceefb4ec88dfbb969
Arielle <ariellescherr@gmail.com>
2020-08-19 19:27:23 -0500

    Stack for functional pattern complete

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,43 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var newKey;
+    var keys = Object.keys(storage);
+
+    if (keys.length === 0) {
+      newKey = 0;
+    } else {
+      newKey = parseInt(keys[keys.length - 1]);
+      newKey += 1;
+    }
+
+    JSON.stringify(newKey);
+    storage[newKey] = value;
+
+    return someInstance;
   };
 
   someInstance.pop = function() {
+    var result;
+    var keys = Object.keys(storage);
+
+    if (keys.length === 0) {
+      return {};
+
+    } else if (keys[keys.length - 1]) {
+      result = storage[keys[keys.length - 1]];
+      delete storage[keys[keys.length - 1]];
+      return result;
+    }
+
   };
 
   someInstance.size = function() {
+    if (!Object.keys(storage)) {
+      return 0;
+    } else {
+      return Object.keys(storage).length;
+    }
   };
 
   return someInstance;
