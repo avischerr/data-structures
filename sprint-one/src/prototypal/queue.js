8bb3dc257b5ed79fa09fcc2eb72236a75ee4fb13
Arielle <ariellescherr@gmail.com>
2020-08-20 17:43:04 -0500

    Prototypal complete for stack, in-progress for queue / Functional and Functional Shared complete

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,42 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function (value) {
+    var keys = Object.keys(this);
+    var newKey = keys.length;
+
+    this[newKey] = value;
+    return this;
+  },
+
+  dequeue: function () {
+    var result;
+    var keys = Object.keys(this);
+
+    console.log('before dequeue: ', JSON.stringify(this));
+
+    if (keys.length > 0) {
+      result = this[0];
+      delete this[0];
+      for (var index in this) {
+        newKey = parseInt(index) - 1;
+        newKey = JSON.stringify(newKey);
+        this[newKey] = this[index];
+        delete this[index];
+      }
+    }
+    console.log('after dequeue: ', JSON.stringify(this));
+    return result;
+  },
+
+  size: function () {
+    return Object.keys(this).length;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,39 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  //push
+  push: function(value) {
+    var newKey = Object.keys(this).length;
+
+    this[newKey] = value;
+
+    return this;
+  },
+  //pop
+  pop: function() {
+    var result;
+    var keys = Object.keys(this);
+
+    if (keys.length === 0) {
+      result = null;
+    } else {
+      result = this[keys.length - 1];
+      delete this[keys.length - 1];
+    }
+
+    return result;
+  },
+  //size
+  size: function() {
+    return Object.keys(this).length;
+  }
+};
 
 
